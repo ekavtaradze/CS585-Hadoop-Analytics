197ffd97602997b4adfb121e65b644b53a58cde0
Elene Kavtaradze <39622338+ekavtaradze@users.noreply.github.com>
2020-06-28 02:40:37 -0400

    moving mapper to different file

---
+++ src/main/java/kMeans/KMeansMapper.java
@@ -0,0 +1,65 @@
+package kMeans;
+
+import kMeans.Enums.Centroid;
+import kMeans.Enums.Point;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KMeansMapper extends Mapper<LongWritable, Text, Text, Text> {
+    // private final IntWritable one = new IntWritable(1);
+
+    private Text word = new Text();
+    private List<Centroid> centroids = new ArrayList<Centroid>();
+
+    /**
+     * setup should be called only once before the task begis
+     * reads the centroids dataset, so that each mapped records has access to it
+     *
+     * @param context
+     * @throws IOException
+     */
+    protected void setup(Context context) throws IOException {
+        Configuration conf = context.getConfiguration();
+        Path centroidsPath = new Path(conf.get("centroids"));
+
+        FileSystem fs = FileSystem.get(context.getConfiguration());
+        InputStreamReader inputStream = new InputStreamReader(fs.open(centroidsPath));
+
+        BufferedReader reader = new BufferedReader(inputStream);
+        String line = reader.readLine();
+        while (line != null) {
+            centroids.add(new Centroid(line));
+            line = reader.readLine();
+        }
+        reader.close();
+    }
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        Point point = new Point(line);
+
+        Double minDistance = Double.MAX_VALUE;
+        Centroid minCenter = null;
+
+        for (Centroid centroid : centroids) {
+            Double compare = Distance.findEucledianDistance(centroid, point);
+            if (minDistance > compare) {
+                minDistance = compare;
+                minCenter = centroid;
+            }
+        }
+        context.write(new Text(minCenter.toString()), new Text(minDistance.toString()));
+
+    }
+}
+
