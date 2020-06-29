8d8d2c6711962c570e9e19e761475c12cfe2a536
Elene Kavtaradze <39622338+ekavtaradze@users.noreply.github.com>
2020-06-29 02:08:53 -0400

    testing

--- src/main/java/spatialJoin/SpatialJoinReducer.java
+++ src/main/java/spatialJoin/SpatialJoinReducer.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 
 public class SpatialJoinReducer extends Reducer<Rectangle, Point, Text, Text> {
     public void reduce(Rectangle key, Iterable<Point> values, Context context) throws IOException, InterruptedException {
+        System.out.println("reducer");
         for (Point point : values) {
+            System.out.println(point.toString());
             context.write(new Text(key.toString()), new Text(point.toString()));
         }
     }
